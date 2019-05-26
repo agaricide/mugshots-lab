6252e1969ff0da4bf983aef5327bfe21dba69cb4
agaricide <agaricide@gmail.com>
2019-05-25 22:51:00 -0500

    Implemented new County ingestion setup This should improve potential sampling bias

--- client-accuracy/src/ingest/counties.ts
+++ client-accuracy/src/ingest/counties.ts
@@ -1,13 +1,23 @@
 import "reflect-metadata";
 import * as dotenv from "dotenv";
 import * as mongoose from "mongoose";
-import { MugshotModel } from "../models/Mugshot";
-import { MugshotStream, Mugshot } from "../../../../mugshots-client/dist";
+import { CountyModel } from "../models/County";
+import { CountyIterable } from "../../../../mugshots-client";
+import * as puppeteer from "puppeteer";
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
-(async () => {
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  const counties = await CountyIterable(page);
+  for await (const county of counties) {
+    console.log(county);
+    CountyModel.create(county);
+    await sleep(2000);
+  }
 })();
